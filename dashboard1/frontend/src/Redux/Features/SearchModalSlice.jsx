9e1873808843110a6b7e10e6a0d4c64435d6817b
Dennis Peter <peterdennis573@gmail.com>
2024-09-11 13:02:55 +0300

    Updates

--- dashboard1/frontend/src/Redux/Features/SearchModalSlice.jsx
+++ dashboard1/frontend/src/Redux/Features/SearchModalSlice.jsx
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
 
-    displaySearchModal:false,
+    displaySearchModal:true,
 
 };
 
--- dashboard1/frontend/src/components/SearchModal.jsx
+++ dashboard1/frontend/src/components/SearchModal.jsx
@@ -1,6 +1,16 @@
 import "./search.css";
 import { useSelector, useDispatch } from "react-redux";
 import { hideSearchModal } from "../Redux/Features/SearchModalSlice";
+import { FaSearch } from "react-icons/fa";
+import { MdDashboardCustomize } from "react-icons/md";
+import { FaArrowsDownToPeople } from "react-icons/fa6";
+import { MdGroups } from "react-icons/md";
+import { MdAccountBalance } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { TbReportSearch } from "react-icons/tb";
+import { FaUser } from "react-icons/fa";
+import { FaRegShareSquare } from "react-icons/fa";
+import { IoFileTrayFull } from "react-icons/io5";
 const SearchModal = () => {
   const dispatch = useDispatch();
   const displaySearchModal = useSelector(
@@ -9,6 +19,66 @@ const SearchModal = () => {
   const handleHideSearchModal = () => {
     dispatch(hideSearchModal());
   };
+
+  const searchItems = [
+    {
+      title: "COMMON SHORTCUTS",
+      items: [
+        {
+          item: "Clients",
+          logo: <FaArrowsDownToPeople />,
+        },
+        {
+          item: "Groups",
+          logo: <MdGroups/>,
+        },
+      ],
+    },
+    {
+      title: "CLIENT ACCOUNTS",
+      items: [
+        {
+          item: "Savings Accounts",
+          logo: <MdAccountBalance />,
+        },
+        {
+          item: "Share Accounts",
+          logo: <FaRegShareSquare/>,
+        },
+        {
+          item: "Client Loans / Action Tray",
+          logo: <IoFileTrayFull />,
+        },
+      ],
+    },
+    {
+      title: "SYSTEM",
+      items: [
+        {
+          item: "Change Password",
+          logo: <RiLockPasswordFill />,
+        },
+        {
+          item: "System Users",
+          logo: <FaUser />,
+        },
+      ],
+    },
+    {
+      title: "REPORTS",
+      items: [
+        {
+          item: "Dashboard",
+          logo: <MdDashboardCustomize />,
+        },
+        {
+          item: "Reports",
+          logo: <TbReportSearch/>,
+        },
+      ],
+    },
+  ];
+
   return (
     <div
       className={`modal search ${displaySearchModal ? "active" : ""}`}
@@ -19,22 +89,29 @@ const SearchModal = () => {
           &times;
         </button>
         <div className="modal-wrapper-container">
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum iure
-            soluta debitis assumenda dolore? Adipisci cupiditate aliquam, culpa
-            ipsum esse maiores vitae corporis ducimus nobis, debitis a sint
-            reiciendis! Vel enim dolores dolor voluptatum sunt, ut assumenda
-            velit vitae dolorum corrupti modi tempore quas temporibus ipsam, sit
-            iste non! Explicabo!
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum iure
-            soluta debitis assumenda dolore? Adipisci cupiditate aliquam, culpa
-            ipsum esse maiores vitae corporis ducimus nobis, debitis a sint
-            reiciendis! Vel enim dolores dolor voluptatum sunt, ut assumenda
-            velit vitae dolorum corrupti modi tempore quas temporibus ipsam, sit
-            iste non! Explicabo!
-          </p>
+          <div className="modal-header">
+            <div className="input-group">
+              <input type="text" name="" id="" placeholder="Search..." />
+              <span>
+                <FaSearch />
+              </span>
+            </div>
+          </div>
+          <div className="modal-body">
+            <div className="grid">
+              {searchItems.map((item, index) => (
+                <div className="col" key={index}>
+                  <p className="medium-header">{item.title}</p>
+                  {item.items.map((itemOfItems, index) => (
+                    <p key={index} className="inner-header">
+                      <span>{itemOfItems.logo}</span>
+                      {itemOfItems.item}
+                    </p>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
