adbb8584504fe081858ce4c472d75a92b62ec6bc
Dennis Peter <peterdennis573@gmail.com>
2024-09-16 19:50:15 +0300

    Created a separate store for icons instead of constantly calling them

---
+++ dashboard1/frontend/src/icons.jsx
@@ -0,0 +1,17 @@
+import { FaUpload, FaSearch, FaPrint, FaRegEdit } from "react-icons/fa";
+import { IoIosCloudDownload } from "react-icons/io";
+import { GrColumns } from "react-icons/gr";
+import { IoFilterSharp } from "react-icons/io5";
+
+const icons = {
+  search: <FaSearch />,
+  printIcon: <FaPrint />,
+  uploadIcon: <FaUpload />,
+  downloadIcon: <IoIosCloudDownload />,
+  columnsIcon: <GrColumns />,
+  filterIcon: <IoFilterSharp />,
+  editIcon: <FaRegEdit />,
+};
+
+export default icons;
+    
